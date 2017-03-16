8660ef3f347a4432e96264c63c2570418727bb2f
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-03-16 11:17:40 -0300

    Login menos fracassado

--- FireBase Auth/index.js
+++ FireBase Auth/index.js
@@ -3,20 +3,43 @@ var fs		= require('fs');
 var url		= require('url');
 var path	= require('path');
 var express = require('express');
+var bodyParser	= require('body-parser');
 var app 	= express();
 
+//Utilizar o BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var userLogado;
 var index	= 'index.html';
 var signout	= 'signout.html';
+var logged = 'logged.html';
+
+var logado = true;
 
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, index));
 });
 
+app.post("/postUser", function(req, res) {
+	console.log(req.body.name);
+	userLogado = req.body;
+	res.send("");
+});
+
+app.get("/redirectLogin", function(req, res) {
+	res.sendFile(path.join(__dirname, logged));
+});
+
+app.post("/login", function(req, res) {
+	console.log("LOGGIN");
+	res.setHeader('Content-Type', 'application/json');
+	res.json(logado);
+});
+
 app.get("/signout", function(req, res) {
-	if(req.url !== "/signout") {
-		console.log("SIGNOUT");
-		return res.redirect('/signout');
-	}
+	console.log("SAIR");
+	res.sendFile(path.join(__dirname, signout));
 });
 
 app.listen(3000);
