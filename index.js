335d229e3e04356f631d730fbe8dce8a41fb9aff
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-06-30 15:24:57 -0300

    testes db

--- index.js
+++ index.js
@@ -3,20 +3,47 @@ var http			= require('http');
 var fs				= require('fs');
 var url				= require('url');
 var path			= require('path');
-var express 		= require('express');
+var express			= require('express');
+var mysql			= require('mysql');
 var bodyParser		= require('body-parser');
 var session			= require('express-session');
 var app 			= express();
 
-//MySQL
-var mysql		= require('mysql');
-var connection	= mysql.createConnection({
+//*****MySQL*****//
+var connection;
+var db_config = {
 	host	: 'us-cdbr-iron-east-03.cleardb.net',
 	user	: 'bae2902327afa9',
 	password: 'aa09eff1',
 	database: 'heroku_c4f1d12a0ffd9dd'
-});
+};
 
+//Conecta ao Banco de Dados
+function handleDisconnect() {
+	console.log('1. connecting to db:');
+	connection = mysql.createConnection(db_config);
+
+	connection.connect(function(err) {
+		if(err) {
+			console.log('2. error when connecting to db:', err);
+			setTimeout(handleDisconnect, 1000);
+		}
+	});
+
+	connection.on('error', function(err) {
+		console.log('3. db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+
+//Inicia DB
+handleDisconnect();
+
+//*****DEPENDENCIAS*****//
 //Utilizar o BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -29,18 +56,8 @@ app.use(express.static('./', {
 	index: 'html/login/index.html'
 }));
 
-//Conecta ao Banco de Dados
-connection.connect(function(err) {
-	if(!err){
-		console.log("Database connected...");
-	}
-	else{
-		console.log("Error connecting to database...");
-	}
-});
-
 /*********************************PAGINAS********************************/
-var index	= '/html/index.html';
+var index = '/html/index.html';
 var login = '/html/login/index.html';
 
 /*************************VARIAVEIS DE EXECUCAO**************************/
