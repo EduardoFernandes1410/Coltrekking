4490c1bc05b04400b48d17339a3f763513cb10ba
Eduardo Fernandes <eduardo.militao1410@gmail.com>
2017-10-10 21:51:39 -0300

    configs

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+	connectionLimit : 300,
+	host	: 'us-cdbr-iron-east-05.cleardb.net',
+	user	: 'b8da9162116c36',
+	password: '089dba03a9f1701',
+	database: 'heroku_d85ec50b8eb7067',
+	debug	:  false
+};
+
+module.exports = config;
