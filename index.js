9bd3fef3616b95285827cf2d4ac383377db722e6
guildeassis <guildeassis@gmail.com>
2018-05-12 10:52:01 -0300

    teste servidor

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.use(express.static('./', {
 //*****MySQL*****//
 var pool = mysql.createPool({
 	connectionLimit : 300,
-	host	: process.env.DB_HOST,
-	user	: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+	host	: 'us-cdbr-iron-east-05.cleardb.net',
+	user	: 'b8da9162116c36',
+	password: '6b5d5bfbfa852a2',
+	database: 'heroku_d85ec50b8eb7067',
 	debug	: false
 });
 
