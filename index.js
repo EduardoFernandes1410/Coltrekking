63fde29fa0454d4137c1b02516128ccedd5619b9
guildeassis <guildeassis@gmail.com>
2018-05-12 10:55:16 -0300

    normal

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.use(express.static('./', {
 //*****MySQL*****//
 var pool = mysql.createPool({
 	connectionLimit : 300,
-	host	: 'us-cbdbr-iron-east-05.cleardb.net',
-	user	: 'bb8da9162116c36',
-	password: 'b6b5d5bfbfa852a2',
-	database: 'heroku_bd85ec50b8eb7067',
+	host	: process.env.DB_HOST,
+	user	: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 	debug	: false
 });
 
