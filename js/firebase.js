877376b17ff3c4617a3c38d63063d032a3476288
Guilherme Assis <guildeassis@gmail.com>
2018-09-20 21:51:40 -0300

    Revert "Countdown funcionando!!!"
    This reverts commit a4c1c07f6e683ba5824b2651fe20c6fd7f0f5e1d.

--- js/firebase.js
+++
@@ -1,9 +0,0 @@
-// Inicializa Firebase
-var config = {
-	apiKey: "AIzaSyDWuZXZioUNujSBLLOtH1tRF8IWCIqMH4g",
-	authDomain: "coltrekking-b81da.firebaseapp.com",
-	databaseURL: "https://coltrekking-b81da.firebaseio.com",
-	storageBucket: "coltrekking-b81da.appspot.com",
-	messagingSenderId: "1020694218506"
-};
-firebase.initializeApp(config);
